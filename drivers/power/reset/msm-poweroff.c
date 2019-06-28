e860a2ed7c0ff4d5e67ec19ce10129b09730fc35
freak07 <michalechner92@googlemail.com>
2019-06-28 17:47:21 +0200

    msm-poweroff: keep console-ramoops alive
    (cherry picked from commit 6590131991c2073d94cfa048fa87b4b7011a8f96)
    (cherry picked from commit 3f0511f0bde07a5ea36a353faddd216f95e27516)
    (cherry picked from commit 4d7b00b8f289c01b25954782e14759c2564c263d)
    (cherry picked from commit 6ae2e868bebc06f1d2502646229561cd01c97191)
    (cherry picked from commit 3aaffd2bd316f220ebef4c1e1843b43820dfee40)
    (cherry picked from commit 0f6c90b71205eb580fe4f4e657ef7884a9e79307)

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -329,9 +329,9 @@ static void msm_restart_prepare(const char *cmd)
 
 	if (force_warm_reboot)
 		pr_info("Forcing a warm reset of the system\n");
-#else
-	need_warm_reset = get_dload_mode();
-#endif
+		
+	/* keep console-ramoops alive */
+	need_warm_reset = true;
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (force_warm_reboot || need_warm_reset)
