073cb6841deea8ff61b6bc3e7f6e6c99ecfe324c
firemax13 <mydadisbrave2@gmail.com>
2020-10-27 06:07:43 +0000

    Revert "drm/drm_atomic: Boost only if last interaction was within 3s"
    This reverts commit 3aa538811afe01dd836306ac5f45249fb83c6ed8.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -69,6 +69,7 @@ module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
 
 module_param(dynamic_stune_boost, short, 0644);
+
 module_param(dynamic_sched_boost, bool, 0644);
 
 unsigned long last_input_time;
@@ -354,8 +355,6 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	struct boost_drv *b = handle->handler->private;
 
 	__cpu_input_boost_kick(b);
-
-	last_input_time = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
