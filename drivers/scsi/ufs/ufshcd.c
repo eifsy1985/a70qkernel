1fdbcb689c4702ede42c42438b7b908a06fe91c8
firemax13 <mydadisbrave2@gmail.com>
2020-10-27 06:18:52 +0000

    Revert "ufshcd: Never allow clkscale being disabled by init"
    This reverts commit 35b3bcfd44606f23eb58dda9d79ef081ec91cfab.

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -43,8 +43,6 @@
 #include <linux/nls.h>
 #include <linux/of.h>
 #include <linux/blkdev.h>
-#include <linux/suspend.h>
-#include <linux/binfmts.h>
 #include "ufshcd.h"
 #include "ufs_quirks.h"
 #include "unipro.h"
@@ -2422,10 +2420,6 @@ static ssize_t ufshcd_clkscale_enable_store(struct device *dev,
 		return -EINVAL;
 
 	value = !!value;
-
-	if (task_is_booster(current) && !value)
-		value = !value;
-
 	if (value == hba->clk_scaling.is_allowed)
 		goto out;
 
